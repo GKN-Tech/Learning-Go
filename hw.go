b364a3083393f00f9f5066a606e4e394ac07d551
GKN-Tech <gurkiratkaurn.02@gmail.com>
2024-11-14 21:13:44 -0800

    Initial commit hw

---
+++ hw.go
@@ -0,0 +1,14 @@
+// Starting off with "Hello World"
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	msg := "Hello World of Gophers"
+	fmt.Println(msg)
+	fmt.Println(time.Now())
+}
